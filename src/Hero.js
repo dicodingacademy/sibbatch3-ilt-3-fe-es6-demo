27cead4290c6ee86c4f5fdc473244357db1b2013
Dimas Saputra <dimas@dicoding.com>
2022-03-10 17:31:39 +0700

    implement class

--- src/Hero.js
+++ src/Hero.js
@@ -1,13 +1,13 @@
 import Fighter from './Figther.js';
 
-function Hero(name, health, attackPower, defensePower) {
-  Fighter.call(this, name, health, attackPower, defensePower);
-}
+class Hero extends Fighter {
+  constructor(name, health, attackPower, defensePower) {
+    super(name, health, attackPower, defensePower);
+  }
 
-Hero.prototype = Object.create(Fighter.prototype);
-Hero.prototype.constructor = Hero;
-Hero.prototype.sayHello = function () {
-  console.log(`My name is ${this.name}! I am a mighty hero!`);
-};
+  sayHello() {
+    console.log(`My name is ${this.name}! I am a mighty hero!`);
+  }
+}
 
 export default Hero;
--- src/Monster.js
+++ src/Monster.js
@@ -1,10 +1,9 @@
 import Fighter from './Figther.js';
 
-function Monster(name, health, attackPower, defensePower) {
-  Fighter.call(this, name, health, attackPower, defensePower);
+class Monster extends Fighter {
+  constructor(name, health, attackPower, defensePower) {
+    super(name, health, attackPower, defensePower);
+  }
 }
-
-Monster.prototype = Object.create(Fighter.prototype);
-Monster.prototype.constructor = Monster;
 
 export default Monster;
