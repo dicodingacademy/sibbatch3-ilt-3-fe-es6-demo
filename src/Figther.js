d72b8a8a1f6fb1cdc825befcdd9edfbbbd074c6b
Dimas Saputra <dimas@dicoding.com>
2022-03-10 17:28:28 +0700

    implement template literals

--- src/Figther.js
+++ src/Figther.js
@@ -25,15 +25,16 @@ Fighter.prototype.attack = function (enemy) {
   if (damage > 0) {
     enemy.health -= damage;
   }
-  console.log(this.name + ' attacks ' + enemy.name + ' for ' + damage + ' damage.');
+  
+  console.log(`${this.name} attacks ${enemy.name} for ${damage} damage.`);
 
   if (enemy.health <= 0) {
     enemy.isDie = true;
-    console.log(enemy.name + ' is dead.');
+    console.log(`${enemy.name} is dead.`);
     return;
   }
 
-  console.log(enemy.name + ' has ' + enemy.health + ' health left.');
+  console.log(`${enemy.name} has ${enemy.health} health left.`);
 };
 
 export default Fighter;
--- src/Hero.js
+++ src/Hero.js
@@ -7,7 +7,7 @@ function Hero(name, health, attackPower, defensePower) {
 Hero.prototype = Object.create(Fighter.prototype);
 Hero.prototype.constructor = Hero;
 Hero.prototype.sayHello = function () {
-  console.log('My name is ' + this.name + '! I am a mighty hero!');
+  console.log(`My name is ${this.name}! I am a mighty hero!`);
 };
 
 export default Hero;
